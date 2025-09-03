fe2f26e331709f942ae85e2222c173b7021f71fb
Ulises Efrain Hernandez <programacionarte31@gmail.com>
2025-09-02 23:39:31 -0600

    icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,32 +3,32 @@
 import { Box, Typography } from "@mui/material";
 
 export default function Footer() {
-    return (
-        <Box
-            component="footer"
-            sx={{
-                backgroundColor: "black",
-                color: "white",
-                py: 4,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                gap: 2,
-                mt: 6,
-            }}
-        >
-            {/* Contenedor principal */}
-            <Box
-                sx={{
-                    width: "100%",
-                    maxWidth: "1200px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: 2,
-                }}
-            >
-                {/* <Typography
+  return (
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: "black",
+        color: "white",
+        py: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 2,
+        mt: 6,
+      }}
+    >
+      {/* Contenedor principal */}
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "1200px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <Typography
           variant="body1"
           sx={{
             textAlign: "center",
@@ -37,33 +37,41 @@ export default function Footer() {
           }}
         >
           © 2025 Ale Vezquez
-        </Typography> */}
+        </Typography>
 
-                <Box sx={{ display: "flex", gap: 3, justifyContent: "center", mt: 3 }}>
-                    <a
-                        href="https://www.instagram.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={{ display: "inline-block" }}
-                    >
-                        <svg width="50" height="50" fill="white">
-                            <use href="#instagram-icon" />
-                        </svg>
-                    </a>
+        {/* Iconos con links */}
+        <Box sx={{ display: "flex", gap: 3, justifyContent: "center", mt: 3 }}>
+          {/* Instagram */}
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ display: "inline-block" }}
+          >
+            <img
+              src="/images/instagram.png"
+              alt="Instagram"
+              width={50}
+              height={50}
+              style={{ display: "block" }}
+            />
+          </a>
 
-                    <a
-                        href="https://www.twitter.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={{ display: "inline-block" }}
-                    >
-                        <svg width="50" height="50" fill="white">
-                            <use href="#twitter-icon" />
-                        </svg>
-                    </a>
-                </Box>
-
-            </Box>
+          {/* Gmail */}
+          <a
+            href="mailto:tuemail@gmail.com"
+            style={{ display: "inline-block" }}
+          >
+            <img
+              src="/images/gmail2.png"
+              alt="Gmail"
+              width={50}
+              height={50}
+              style={{ display: "block" }}
+            />
+          </a>
         </Box>
-    );
+      </Box>
+    </Box>
+  );
 }
